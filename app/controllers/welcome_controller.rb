fe742d302b0e0ea7c448387c6b8a56be54c680d3
Frank Sommers <ssommers21@gmail.com>
2016-11-01 22:39:26 -0700

    cleans up css mvp reached about to add ajax

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,8 +3,7 @@ class WelcomeController < ApplicationController
   def index
   	 if current_user 
      redirect_to articles_path
-   else 
-    redirect_to new_user_registration_path
-   end
+  
+     end
   end
 end
